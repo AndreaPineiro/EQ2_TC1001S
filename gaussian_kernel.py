9c805b07d858ab4d6b03c3473332954ae64fff31
RafaelHinojosa <rafael.zeo@gmail.com>
2021-05-06 22:08:21 -0500

    Kernel 2D listo

--- gaussian_kernel.py
+++ gaussian_kernel.py
@@ -7,9 +7,16 @@ def gaussian_function (x, sigma):
 
 def gaussian_kernel(size, sigma=1, verbose=False):
     g_kernel_1d = np.linspace(-(size // 2), size // 2, size);
+    
     for i in range(size):
         g_kernel_1d[i] = gaussian_function(g_kernel_1d[i], sigma)
     print(g_kernel_1d)
+
+    g_kernel_2d = np.outer(g_kernel_1d.T, g_kernel_1d.T)
+    print(g_kernel_2d)
+
+    g_kernel_2d = g_kernel_2d * 1 / g_kernel_2d.max()
+    print(g_kernel_2d)
 
 kernel_size = int(input('Ingrese el tamaño del kernel: '));
 gaussian_kernel(kernel_size, sigma=np.sqrt(kernel_size), verbose=False);
