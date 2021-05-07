fdfe9cb5b35281ebc601598fb5d07cb190b27e86
RafaelHinojosa <rafael.zeo@gmail.com>
2021-05-06 22:04:29 -0500

    Kernel 1D listo

--- gaussian_kernel.py
+++ gaussian_kernel.py
@@ -5,4 +5,11 @@ import matplotlib.pyplot as plt
 def gaussian_function (x, sigma): 
 	return 1 / (np.sqrt(2 * np.pi) * sigma) * np.e ** (-(np.power((x/sigma), 2) / 2))
 
-print(gaussian_function(4, 1))
+def gaussian_kernel(size, sigma=1, verbose=False):
+    g_kernel_1d = np.linspace(-(size // 2), size // 2, size);
+    for i in range(size):
+        g_kernel_1d[i] = gaussian_function(g_kernel_1d[i], sigma)
+    print(g_kernel_1d)
+
+kernel_size = int(input('Ingrese el tamaño del kernel: '));
+gaussian_kernel(kernel_size, sigma=np.sqrt(kernel_size), verbose=False);
