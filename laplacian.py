72bd427911e243eebfc95767e60d5839bf71af9b
Joaquinzer <joaquin@DESKTOP-0MAU4T7.localdomain>
2021-05-07 11:35:49 -0500

    Laplacian filter

---
+++ laplacian.py
@@ -0,0 +1,21 @@
+import numpy as np
+import math
+import cv2
+import matplotlib.pyplot as plt
+from convolution import convolution
+
+def laplacian(image, verbose=False):
+    kernel = np.array([[0, 0, -1, 0, 0], [0, -1, -2, -1, 0], [-1, -2, -16, -2, -1], [0, -1, -2, -1, 0], [0, 0, -1, 0, 0]])
+
+    image_lap = convolution(image, kernel, False)
+
+    if verbose:
+        plt.imshow(image_lap, cmap='gray')
+        plt.title('laplacian')
+        plt.show()
+
+    return image
+
+image = cv2.imread("turquia.jpg")
+laplacian(image, True)
+
