122f4124b5f10ce3bebe5b6ae0761896c44f138e
Joaquinzer <joaquin@DESKTOP-0MAU4T7.localdomain>
2021-05-07 12:04:02 -0500

    laplacian comentado

--- laplacian.py
+++ laplacian.py
@@ -5,10 +5,13 @@ import matplotlib.pyplot as plt
 from convolution import convolution
 
 def laplacian(image, verbose=False):
+    #se define el kernel usado para este filtro
     kernel = np.array([[0, 0, -1, 0, 0], [0, -1, -2, -1, 0], [-1, -2, -16, -2, -1], [0, -1, -2, -1, 0], [0, 0, -1, 0, 0]])
 
+    #se hace la convolucion usando el kernel declarado
     image_lap = convolution(image, kernel, False)
 
+    #se muestra la imagen
     if verbose:
         plt.imshow(image_lap, cmap='gray')
         plt.title('laplacian')
